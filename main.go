870dec5151d913e2a3a62105c7093f1e33f87c34
kenigbolo <expensivestephen@hotmail.com>
2018-06-30 00:59:32 +0300

    Initial server file with commented lines server handling

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "net/http"
+
+// type myHandler struct {
+// 	greeting string
+// }
+
+// func (mh myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	w.Write([]byte(fmt.Sprintf("%v world", mh.greeting)))
+// }
+
+func main() {
+	// http.Handle("/", &myHandler{greeting: "Hello"})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// w.Write([]byte("Hello Go!"))
+		http.ServeFile(w, r, "public"+r.URL.Path)
+	})
+	http.ListenAndServe(":8000", nil)
+}
