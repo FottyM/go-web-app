c0f8f927c13fdb6ec44013a23683ad9e044f9ba3
kenigbolo <expensivestephen@hotmail.com>
2018-06-30 13:36:43 +0300

    extract template creating function into separate package, make view more precise

--- main.go
+++ main.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-	"html/template"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 
+	"github.com/kenigbolo/go-web-app/templates"
 	"github.com/kenigbolo/go-web-app/viewmodel"
 )
 
 var filePath = "../src/github.com/kenigbolo/go-web-app/"
 
 func main() {
-	templates := populateTemplates()
+	templates := templates.PopulateTemplates(filePath)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		requestedFile := r.URL.Path[1:]
 		template := templates[requestedFile+".html"]
@@ -21,8 +19,10 @@ func main() {
 		switch requestedFile {
 		case "shop":
 			context = viewmodel.NewShop()
+		case "stand_locator":
+			context = viewmodel.NewStandLocator()
 		default:
-			context = viewmodel.NewBase()
+			context = viewmodel.NewHome()
 		}
 		if template != nil {
 			err := template.Execute(w, context)
@@ -30,43 +30,10 @@ func main() {
 				log.Println(err)
 			}
 		} else {
-			w.WriteHeader(http.StatusNotFound)
+			w.WriteHeader(404)
 		}
 	})
 	http.Handle("/img/", http.FileServer(http.Dir(filePath+"public")))
 	http.Handle("/css/", http.FileServer(http.Dir(filePath+"public")))
 	http.ListenAndServe(":8000", nil)
-}
-
-func populateTemplates() map[string]*template.Template {
-	result := make(map[string]*template.Template)
-	basePath := filePath + "templates"
-	layout := template.Must(template.ParseFiles(basePath + "/_layout.html"))
-	template.Must(layout.ParseFiles(basePath+"/_header.html", basePath+"/_footer.html"))
-	dir, err := os.Open(basePath + "/content")
-	if err != nil {
-		panic("Failed to open template blocks directory: " + err.Error())
-	}
-	fis, err := dir.Readdir(-1)
-	if err != nil {
-		panic("Failed to read contents of content directory: " + err.Error())
-	}
-	for _, fi := range fis {
-		f, err := os.Open(basePath + "/content/" + fi.Name())
-		if err != nil {
-			panic("Failed to open template '" + fi.Name() + "'")
-		}
-		content, err := ioutil.ReadAll(f)
-		if err != nil {
-			panic("Failed to read content from file '" + fi.Name() + "'")
-		}
-		f.Close()
-		tmpl := template.Must(layout.Clone())
-		_, err = tmpl.Parse(string(content))
-		if err != nil {
-			panic("Failed to parse contents of '" + fi.Name() + "' as template")
-		}
-		result[fi.Name()] = tmpl
-	}
-	return result
 }
---
+++ templates/template.go
@@ -0,0 +1,41 @@
+package templates
+
+import (
+	"html/template"
+	"io/ioutil"
+	"os"
+)
+
+// PopulateTemplates Engine
+func PopulateTemplates(filePath string) map[string]*template.Template {
+	result := make(map[string]*template.Template)
+	basePath := filePath + "templates"
+	layout := template.Must(template.ParseFiles(basePath + "/_layout.html"))
+	template.Must(layout.ParseFiles(basePath+"/_header.html", basePath+"/_footer.html"))
+	dir, err := os.Open(basePath + "/content")
+	if err != nil {
+		panic("Failed to open template blocks directory: " + err.Error())
+	}
+	fis, err := dir.Readdir(-1)
+	if err != nil {
+		panic("Failed to read contents of content directory: " + err.Error())
+	}
+	for _, fi := range fis {
+		f, err := os.Open(basePath + "/content/" + fi.Name())
+		if err != nil {
+			panic("Failed to open template '" + fi.Name() + "'")
+		}
+		content, err := ioutil.ReadAll(f)
+		if err != nil {
+			panic("Failed to read content from file '" + fi.Name() + "'")
+		}
+		f.Close()
+		tmpl := template.Must(layout.Clone())
+		_, err = tmpl.Parse(string(content))
+		if err != nil {
+			panic("Failed to parse contents of '" + fi.Name() + "' as template")
+		}
+		result[fi.Name()] = tmpl
+	}
+	return result
+}
